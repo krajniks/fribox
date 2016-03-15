7c374ca4f95c3d22d12bddb127eb16fd96d60ffd
Simon Krajnik <simkrajnik@gmail.com>
2016-03-15 17:45:42 +0000

    naloga 3

--- public/skripta.js
+++ public/skripta.js
@@ -11,10 +11,13 @@ window.addEventListener('load', function() {
 	
 	document.querySelector("#nalozi").addEventListener("click", prizgiCakanje);
 	
+	
+	
 	//Pridobi seznam datotek
 	var pridobiSeznamDatotek = function(event) {
 		prizgiCakanje();
 		var xhttp = new XMLHttpRequest();
+
 		xhttp.onreadystatechange = function() {
 			if (xhttp.readyState == 4 && xhttp.status == 200) {
 				var datoteke = JSON.parse(xhttp.responseText);
@@ -42,7 +45,11 @@ window.addEventListener('load', function() {
 				ugasniCakanje();
 			}
 		};
+		xhttp.open("GET","/datoteke",true);
+		xhttp.send();
 	}
+	
+	pridobiSeznamDatotek();
 	
 	var brisi = function(event) {
 		prizgiCakanje();
